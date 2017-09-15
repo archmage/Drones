39a7accfd4c51dcd4dbee7e0f6b0d9cd625bcf10
Kara Valentine <kara+gitkraken@archmage.me>
2017-09-16 03:52:54 +1000

    Remove unnecessary Main, tidy up some constructors

--- src/main/scala/com/archmage/drones/Structure.scala
+++ src/main/scala/com/archmage/drones/Structure.scala
@@ -3,7 +3,7 @@ package com.archmage.drones
 import com.archmage.drones.Drone.Gather
 import com.archmage.drones.components.Geo
 
-case class Structure(geo: Geo, scrap: Int = 0) {
+case class Structure(geo: Geo = Geo(), scrap: Int = 0) {
 
   def act(world: World): Structure = {
     val drones = world.drones.filter(d => {
